05b730946f887d316aae432a50d51b1516b39a6b
keerthiAppspire <keerthi@appspiresolutions.in>
2026-08-10 16:45:10 +0530

    Using a Store in a Component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { useCounterStore } from './stores/counter'
+import { storeToRefs } from 'pinia';
 const counter = useCounterStore()
+const  {count,doubled}=storeToRefs(counter)
+const {increment,reset}=counter
 </script>
 <template>
-  <h1>Count: {{ counter.count }}</h1>
-  <h2>Doubled: {{ counter.doubled }}</h2>
+  <h1>Count: {{ count }}</h1>
+  <h2>Doubled: {{ doubled }}</h2>
   <div class="buttons">
-      <button @click="counter.increment">Increment</button>
-      <button @click="counter.reset">Reset</button>
+      <button @click="increment">Increment</button>
+      <button @click="reset">Reset</button>
   </div>
 </template>
 <style scoped>
